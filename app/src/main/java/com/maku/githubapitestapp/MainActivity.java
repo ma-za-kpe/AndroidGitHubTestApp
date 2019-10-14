854d5c70cce06f15b1d132d440aba7551a8014cc
MAZAKPE <makpalyy@gmail.com>
2019-10-14 10:55:21 +0200

    generate the initial project files

---
+++ app/src/main/java/com/maku/githubapitestapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maku.githubapitestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
